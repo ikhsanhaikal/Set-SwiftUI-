1934e89870b5ea1140fedd7557648b4489319a55
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-02-06 15:48:13 +0700

    Initial Commit

---
+++ Set(SwiftUI)/Set_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Set_SwiftUI_App.swift
+//  Set(SwiftUI)
+//
+//  Created by Ikhsan on 06/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Set_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
