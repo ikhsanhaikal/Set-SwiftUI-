0bfb7f11d35727904444d1212487c90f85785f01
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-02-11 22:52:37 +0700

    add main logic of game set
    - choose card method
      * save picked cards
      * remove picked cards that was match or mis-match
    - check method for comparing content of three selected card

--- Set(SwiftUI)/Model.swift
+++ Set(SwiftUI)/Model.swift
@@ -13,6 +13,8 @@ struct Model<SetShape, SetColor, SetNumber, SetShading> where SetShape: Equatabl
     var decks: [Card]
     
     var playingCard: [Card] = []
+    var currentlyChosenCards: [Int] = []
+    var wasSet: Bool?
     
     init(shapes: Array<SetShape>, colors: Array<SetColor>, numbers: Array<SetNumber>, shadings: Array<SetShading> ) {
         decks = []
@@ -30,22 +32,60 @@ struct Model<SetShape, SetColor, SetNumber, SetShading> where SetShape: Equatabl
         playingCard = decks.getAndRemove(first: 12)
     }
     
-//    func choose(card: Card) {
-//        if let chosenCard = playingCard.firstIndex(where: {$0 == card}) {
-//            set cards[chosenCard] status to true or false
-//            if chosenCard count == 3 {
-//                do some calculation if it match record it
-//                                    else record it also
-//            } else if chosenCard count > 3 {
-//                based on record
-//                if it was match remove from playing card
-//                else unselected those 3 card
-//            }
-//                        
-//        }
-//    }
-//    
+    private func check<SetContent: Equatable>(x: SetContent, y: SetContent, z: SetContent) -> Bool {
+        if x == y {
+            if y == z {
+                return true
+            }
+            return false
+        } else {
+            if y == z {
+                return false
+            }
+            return !(x == z)
+        }
+    }
     
+    private func check(suspect: Array<Int>) -> Bool {
+        let x = playingCard[suspect[0]]
+        let y = playingCard[suspect[1]]
+        let z = playingCard[suspect[2]]
+        
+        if check(x: x.shape, y: y.shape, z: z.shape) {
+            if check(x: x.number, y: y.number, z: z.number) {
+                if check(x: x.shading, y: y.shading, z: z.shading) {
+                    if check(x: x.color, y: y.color, z: z.color) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+    
+    mutating func choose(_ card: Card) {
+        if let chosenCard = playingCard.firstIndex(where: {$0.id == card.id}) {
+            if currentlyChosenCards.contains(chosenCard) {
+                currentlyChosenCards.removeAll(where: {$0 == chosenCard})
+                return
+            }
+            currentlyChosenCards.append(chosenCard)
+            print(currentlyChosenCards)
+            if currentlyChosenCards.count == 3 {
+                wasSet = check(suspect: currentlyChosenCards)
+                print("is set: \(String(wasSet!))")
+            } else if currentlyChosenCards.count > 3 {
+                if let result = wasSet, result {
+                    currentlyChosenCards.forEach {
+                        playingCard[$0] = decks.removeFirst()
+                    }
+                }
+                currentlyChosenCards.removeFirst(3)
+                wasSet = nil
+            }
+        }
+    }
+        
     
     struct Card: Identifiable {
         var id = UUID()
