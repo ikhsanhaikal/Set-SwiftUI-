e2c85380c70a41c898f32206057154fb1e20db69
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-02-11 23:07:15 +0700

    create a striped(rectangels pattern) custom shape
    plus add striped extension to Shape protocol

---
+++ Set(SwiftUI)/StripedPattern.swift
@@ -0,0 +1,30 @@
+//
+//  StripedPattern.swift
+//  Set(SwiftUI)
+//
+//  Created by Ikhsan on 11/02/2022.
+//
+
+import SwiftUI
+
+struct StripedPattern: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        var posX: CGFloat = 0
+        while posX <= rect.maxX {
+            path.addRect(CGRect(x: posX, y: 0.0, width: rect.width * 0.03, height: rect.height))
+            posX += rect.width * 0.10
+        }
+        return path
+    }
+}
+
+extension Shape {
+    func striped() -> some View {
+        ZStack {
+            StripedPattern()
+            self.stroke(lineWidth: 3)
+        }
+        .clipShape(self)
+    }
+}
